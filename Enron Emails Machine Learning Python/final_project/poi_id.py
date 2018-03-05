c58ce0f5e2d2ac6b593cbf8ef8632f21ee05b78f
lincont <tania.lincoln@gmail.com>
2018-03-04 21:13:14 -0600

    improved predictions and features

--- Enron Emails Machine Learning Python/final_project/poi_id.py
+++ Enron Emails Machine Learning Python/final_project/poi_id.py
@@ -30,10 +30,9 @@ if __name__ == "__main__":
     from sklearn.tree import DecisionTreeClassifier
     from sklearn.linear_model import LogisticRegression
     from sklearn.model_selection import GridSearchCV
-    from sklearn.preprocessing import MinMaxScaler
     from sklearn.decomposition import PCA
     from sklearn.feature_selection import SelectKBest
-    from sklearn.preprocessing import MinMaxScaler
+    from sklearn.preprocessing import RobustScaler
 
     sys.path.append("../../ud120-projects-master/tools/")
 
@@ -78,16 +77,11 @@ if __name__ == "__main__":
         data_dict[k]['ratio_of_to_messages_poi'] = ratio_builder(data_dict[k]['from_poi_to_this_person'], data_dict[k]['to_messages'])
         data_dict[k]['ratio_of_from_messages_poi'] = ratio_builder(data_dict[k]['from_this_person_to_poi'], data_dict[k]['from_messages'])
 
-        # the input values will be excluded later since there are too many nulls
-        # made boolean values instead, perhaps the actual values are not as important
-        data_dict[k]['has_deferral_payments_bool'] = has_value(data_dict[k]['deferral_payments'])
-        data_dict[k]['has_deferred_income_bool'] = has_value(data_dict[k]['deferred_income'])
-        data_dict[k]['has_director_fees_bool'] = has_value(data_dict[k]['director_fees'])
-        data_dict[k]['has_loan_advances_bool'] = has_value(data_dict[k]['loan_advances'])
-        data_dict[k]['has_long_term_incentive_bool'] = has_value(data_dict[k]['long_term_incentive'])
-        data_dict[k]['has_restricted_stock_deferred_bool'] = has_value(data_dict[k]['restricted_stock_deferred'])
-
     ### feature list creation
+    ### this list was decided after doing
+    ### test and learn in the Enron Jupyter Notebook
+    ### the features developed ended up de-valueing the
+    ### the predictions
     features_list = [
         'poi',
         'salary',
@@ -102,15 +96,7 @@ if __name__ == "__main__":
         'from_messages',
         'other',
         'from_this_person_to_poi',
-        'from_poi_to_this_person',
-        'has_deferral_payments_bool',
-        'has_long_term_incentive_bool',
-        'has_restricted_stock_deferred_bool',
-        'has_loan_advances_bool',
-        'has_deferred_income_bool',
-        'has_director_fees_bool',
-        'ratio_of_to_messages_poi',
-        'ratio_of_from_messages_poi']
+        'from_poi_to_this_person']
 
     for k in data_dict:
         for f in features_list:
@@ -125,18 +111,19 @@ if __name__ == "__main__":
     labels, features = targetFeatureSplit(data)
 
     ### split the dataset into train and test partitions
+    ### slicing this into a third worked the best.
+    ### various values were tried in the notebook
     from sklearn.cross_validation import train_test_split
     features_train, features_test, labels_train, labels_test = \
-        train_test_split(features, labels, test_size=0.3, random_state=42)
+        train_test_split(features, labels, test_size=0.33, random_state=42)
 
     ### Using the initial model candidate research
     ### from the Jupyter notebook, I've selected
     ### KNeighbors, SVC, and RandomForest for more investigation
     
-    ### scaling used
-    scalers = MinMaxScaler()
+    ### Try various models
+    scalers = RobustScaler()
 
-    ### reduction methods
     select_reduction = []
 
     pca_parameters = [
@@ -144,11 +131,10 @@ if __name__ == "__main__":
     select_reduction.append(['pca', PCA(), pca_parameters])
 
     kbest_parameters = [
-        {'reducers__k': [1, 2, 3, 4, 5]},]
+        {'reducers__k': [3, 5, 7, 11, 13]},]
     select_reduction.append(['kbest', SelectKBest(), kbest_parameters])
 
 
-    ### build model permutation features
     candidates = []
 
     ### Logistic Regression
@@ -172,24 +158,18 @@ if __name__ == "__main__":
 
     ### KNN
     knn_parameters = [
-        {'clf__n_neighbors': [1, 3, 5, 10, 20]},
-        {'clf__n_jobs':[1,3,5,7]},]
+        {'clf__n_neighbors': [1, 3, 5, 7, 10, 11, 20]},
+        {'clf__n_jobs':[1, 3, 5, 7, 9]},]
     candidates.append(['kNN', KNeighborsClassifier(), knn_parameters])
 
-    ### build the models
     for name, model, parameters in candidates:
         for r_name, r_model, r_parameters in select_reduction:
-
-            # build a pipeline
+            print (name + r_name)
             pipe = Pipeline([('scalers', scalers), ('reducers', r_model), ('clf', model)])
 
-            # use gridsearchcv to iterate through the permutations
-            grid = GridSearchCV(pipe, cv = 5, verbose = 5, scoring='accuracy', n_jobs = 5, param_grid = parameters + r_parameters)
+            grid = GridSearchCV(pipe, cv = 7, verbose = 5, scoring='f1', n_jobs = 7, param_grid = parameters + r_parameters)
 
-            # train model
             clf = grid.fit(features_train, labels_train)
-
-            # create the predictions
             predictions = clf.predict(features_test)
 
             # show the accuracy
